59f7eaa17205df76ed95ddb3e52fe6930604acfc
JDCODES26 <junaidghori678@gmail.com>
2026-09-08 11:19:13 +0530

    Add files via upload

---
+++ 198.10.py
@@ -0,0 +1,9 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+
+if a>b:
+ print("a is greater")
+elif b>a:
+ print("b is greater")
+else:
+ print("both are equal")
